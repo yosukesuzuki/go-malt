a9e3212ae6a986cba8b0a0fd0a0ec6987e4ad885
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2014-06-02 07:42:33 +0900

    use Goji framework

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/zenazn/goji/web"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "this is index")
 }
 
-func generalPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "this is ", r.URL.Path[6:])
+func generalPage(c web.C, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "general page, %s!", c.URLParams["key_name"])
 }
 
-func articlePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "this is article ", r.URL.Path[9:])
+func articlePage(c web.C, w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "article, %s!", c.URLParams["key_name"])
 }
--- urls.go
+++ urls.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/zenazn/goji"
 )
 
 func init() {
-	http.HandleFunc("/article/", articlePage)
-	http.HandleFunc("/page/", generalPage)
-	http.HandleFunc("/", index)
+	http.Handle("/", goji.DefaultMux)
+	goji.Get("/article/:key_name", articlePage)
+	goji.Get("/:key_name", generalPage)
+	goji.Get("/", index)
 }
