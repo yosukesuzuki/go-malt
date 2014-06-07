b9d8957a36a14a4b9a05ea9a7b090f64c06b4544
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2014-06-07 16:12:38 +0900

    add template

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/zenazn/goji/web"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "this is index")
+	data := map[string]interface{}{
+		"title":       "top",
+		"description": "this is a starter app for GAE/Go",
+		"body":        "hello world",
+	}
+	t, _ := template.ParseFiles("templates/base.html", "templates/index.html")
+	t.ExecuteTemplate(w, "base", data)
 }
 
 func generalPage(c web.C, w http.ResponseWriter, r *http.Request) {
