a462620295c3190282a04b82c2b38eba808d0414
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2014-06-08 22:03:53 +0900

    remove settings.go, including to template

--- template.go
+++ template.go
@@ -132,6 +132,27 @@ func eq(args ...interface{}) bool {
 	return false
 }
 
+// GetBaseURL return base url for google analytics
+func GetBaseURL() string {
+	//Change This to Your Google Analytics ID
+	url := "goappstarter.appspot.com"
+	return url
+}
+
+// GetGaID return id for google analytics
+func GetGaID() string {
+	//Change This to Your Google Analytics ID
+	gaID := "UA-51746203-1"
+	return gaID
+}
+
+// GetFbAppID returns id for Facebook
+func GetFbAppID() string {
+	//Change This to Your FB App ID
+	fbAppID := "1491547807729755"
+	return fbAppID
+}
+
 var tpls = map[string]*template.Template{
 	"404":          newTemplate("templates/base.html", "templates/404.html"),
 	"index":        newTemplate("templates/base.html", "templates/index.html"),
