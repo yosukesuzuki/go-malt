bbb447306b44079cd361d9a62f1f022449f2bccb
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2014-07-13 23:39:48 +0900

    create / update form

--- media/js/admin.form.js
+++ media/js/admin.form.js
@@ -1,29 +1,65 @@
 $(document).ready(function(){
-    var hashArr = location.hash.split("/");
-    $.getJSON("/admin/rest/schema/"+hashArr[1],function(data){
-        var crudVue = new Vue({
-            el: '#crudContainer',
-            data: {
-                formtitle: data.schema.title,
-                id: data.schema.id
-            }
-        });
-        var crudMethod = hashArr[2];
-        switch(crudMethod){
-            case "list":
-                $.getJSON("/admin/rest/"+hashArr[1],function(listData){
-                    var listVue = new Vue({
-                        el: "#formContainer",
-                        template: "#modelListTable",
-                        data: {
-                            items: listData.items,
-                            modelName: listData.model_name
-                        }
-                    });
-                });
-        }
-    });
     Vue.filter('dateFormat', function (value) {
         return value.slice(0,16)
     });
+    var formApp = {};
+    formApp.drawForm = function(){
+        var hashArr = location.hash.split("/");
+        this.modelName = hashArr[1];
+        this.crudMethod = hashArr[2];
+        this.entityKey = hashArr[3];
+        var that = this;
+        $.getJSON("/admin/rest/schema/"+that.modelName,function(data){
+            var crudVue = new Vue({
+                el: '#crudContainer',
+                data: {
+                    formtitle: data.schema.title,
+                    id: data.schema.id
+                }
+            });
+            var formElementArr = [];
+            $.each(data.schema.properties,function(i,val){formElementArr.push(val)});
+            formElementArr.sort(function(a, b){
+                var x = a.fieldOrder;
+                var y = b.fieldOrder;
+                if (x > y) return 1;
+                if (x < y) return -1;
+                return 0;
+            });
+            var crudMethod = that.crudMethod;
+            switch(crudMethod){
+                case "list":
+                    $.getJSON("/admin/rest/"+that.modelName,function(listData){
+                        var listVue = new Vue({
+                            el: "#formContainer",
+                            template: "#modelListTable",
+                            data: {
+                                items: listData.items,
+                                modelName: listData.model_name
+                            }
+                        });
+                    });
+                    break;
+                case "create":
+                    var createVue = new Vue({
+                        el: "#formContainer",
+                        template: "#modelForm",
+                        data: {
+                                items: formElementArr,
+                        }
+                    });
+                    break;
+                case "update":
+                    var updateVue = new Vue({
+                        el: "#formContainer",
+                        template: "#modelForm",
+                        data: {
+                                items: formElementArr,
+                        }
+                    });
+            }
+        });
+    };
+    formApp.drawForm();
+    window.onhashchange = formApp.drawForm;
 });
