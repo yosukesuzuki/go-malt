a3f6a86962f8ba9fa0a3904af552ffa4b6fde8f9
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2014-07-23 08:19:05 +0900

    delete entity function

--- media/js/admin.form.js
+++ media/js/admin.form.js
@@ -1,29 +1,29 @@
 //extend jquery ajax from http://d.hatena.ne.jp/anatoo/20090527/1243415081
 $.extend({
-	"put" : function (url, data, success, error) {
-		error = error || function() {}; 
-		return $.ajax({
-			"url" : url,
-			"data" : data,
-			"success" : success,
-			"type" : "PUT",
-			"cache" : false,
-			"error" : error,
-			"dataType" : "json"
-		});
-	},
-	"del" : function (url, data, success, error) { 
-		error = error || function() {};
-		return $.ajax({
-			"url" : url,
-			"data" : data,
-			"success" : success,
-			"type" : "DELETE",
-			"cache" : false,
-			"error" : error,
-			"dataType" : "json"
-		});
-	}
+    "put": function(url, data, success, error) {
+        error = error || function() {};
+        return $.ajax({
+            "url": url,
+            "data": data,
+            "success": success,
+            "type": "PUT",
+            "cache": false,
+            "error": error,
+            "dataType": "json"
+        });
+    },
+    "del": function(url, data, success, error) {
+        error = error || function() {};
+        return $.ajax({
+            "url": url,
+            "data": data,
+            "success": success,
+            "type": "DELETE",
+            "cache": false,
+            "error": error,
+            "dataType": "json"
+        });
+    }
 });
 
 $(document).ready(function() {
@@ -73,20 +73,20 @@ $(document).ready(function() {
                 case "list":
                     var offset = parseInt(that.entityKey);
                     var requestUrl = "/admin/rest/" + that.modelName;
-                    if(!isNaN(offset)){
-                        requestUrl += "?offset="+offset
+                    if (!isNaN(offset)) {
+                        requestUrl += "?offset=" + offset
                     }
                     $.getJSON(requestUrl, function(listData) {
                         var nextPage = false;
                         var previousPage = false;
-                        if(listData.has_next){
-                            nextPage ="/admin/form/#/" + that.modelName+"/list/"+listData.next_offset;
+                        if (listData.has_next) {
+                            nextPage = "/admin/form/#/" + that.modelName + "/list/" + listData.next_offset;
                         }
-                        if(!isNaN(offset)){
-                            if(offset - listData.per_page != 0){
-                                previousPage ="/admin/form/#/" + that.modelName+"/list/"+(offset - listData.per_page);
-                            }else{
-                                previousPage ="/admin/form/#/" + that.modelName+"/list";
+                        if (!isNaN(offset)) {
+                            if (offset - listData.per_page != 0) {
+                                previousPage = "/admin/form/#/" + that.modelName + "/list/" + (offset - listData.per_page);
+                            } else {
+                                previousPage = "/admin/form/#/" + that.modelName + "/list";
                             }
                         }
                         var listVue = new Vue({
@@ -95,8 +95,24 @@ $(document).ready(function() {
                             data: {
                                 items: listData.items,
                                 modelName: listData.model_name,
-                                next:nextPage,
-                                previous:previousPage
+                                next: nextPage,
+                                previous: previousPage
+                            },
+                            methods: {
+                                deleteEntity: function(e) {
+                                    console.log(e.targetVM);
+                                    if(window.confirm('Delete this entity?')){
+                                        $.del("/admin/rest/" + that.modelName + "/" + e.targetVM.$data.url,{},function(delResponse) {
+                                            if (delResponse.message == "deleted") {
+                                                location.href = "/admin/form/#/" + that.modelName + "/list/success";
+                                                //e.targetVM.$data;
+                                            } else {
+                                                $("#postAlert").html('<div class="alert alert-danger" role="alert">error deleting data</div>');
+                                            }
+                                        });
+                                        e.targetVM.$remove(e.targetVM.$data);
+                                    }
+                                }
                             }
                         });
                     });
@@ -158,7 +174,7 @@ $(document).ready(function() {
                                             putData[val.frmName] = val.frmValue;
                                         }
                                     });
-                                    $.put("/admin/rest/" + that.modelName +"/"+that.entityKey, putData, function(putResponse) {
+                                    $.put("/admin/rest/" + that.modelName + "/" + that.entityKey, putData, function(putResponse) {
                                         if (putResponse.message == "updated") {
                                             location.href = "/admin/form/#/" + that.modelName + "/list/success";
                                         } else {
