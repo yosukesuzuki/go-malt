db4c2a2f759c647f258f381921fc85bf0f1b8be8
Yosuke Suzuki <yosuke.suzuki@gmail.com>
2014-10-11 22:56:11 +0900

    add template for search result

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 
 	"appengine"
 	"appengine/datastore"
-	"appengine/search"
 	"github.com/gorilla/mux"
 	"github.com/russross/blackfriday"
 )
@@ -50,32 +49,11 @@ func articlePage(w http.ResponseWriter, r *http.Request) error {
 	return executeTemplate(w, "page", 200, data)
 }
 
-func getArticleSearch(w http.ResponseWriter, r *http.Request) map[string]interface{} {
-	c := appengine.NewContext(r)
-	var items ArticleSearchList
-	// Iterate over the results.
-	index, openErr := search.Open("global")
-	if openErr != nil {
-		http.Error(w, openErr.Error(), http.StatusInternalServerError)
+func searchPage(w http.ResponseWriter, r *http.Request) error {
+	data := map[string]interface{}{
+		"title":       "Search",
+		"description": "Search Results",
+		"body":        "",
 	}
-	keyword := r.FormValue("keyword")
-	for t := index.Search(c, keyword, nil); ; {
-		var as ArticleSearch
-		_, err := t.Next(&as)
-		if err == search.Done {
-			break
-		}
-		if err != nil {
-			c.Errorf("fetching next ArticleSearch: %v", err)
-			break
-		}
-		items = append(items, as)
-	}
-	listDataSet := map[string]interface{}{"items": items}
-	return listDataSet
-}
-
-func searchPage(w http.ResponseWriter, r *http.Request) {
-	listDataSet := getArticleSearch(w, r)
-	executeJSON(w, 200, listDataSet)
+	return executeTemplate(w, "searchIndex", 200, data)
 }
